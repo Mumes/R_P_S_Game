c4b1270e01a885456212c5a4d5e38a87f1c0bf8b
Mumes <rodionov.max88@gmail.com>
2018-06-06 21:51:40 +0300

    test changes for github

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 <body>
 <div class="container">
 <h1>Welcome to Rock Paper Scissors</h1>
+<h1>There is a test string to check if github can work fine on both machines</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
